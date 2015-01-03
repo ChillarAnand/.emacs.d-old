309b530270acdf588054154e85e6c8739aa3b964
K3 <anand.r@leucinerichbio.com>
2015-01-03 10:13:35 +0530

    added paredit

--- personal/packages.el
+++ personal/packages.el
@@ -37,6 +37,17 @@
 ;; (setq real-auto-save-interval 5) ;; in seconds
 
 
+;; activate paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'python-mode-hook           #'enable-paredit-mode)
+
+
 ;; org reveal
 ;; (load-file "~/.emacs.d/personal/prelude-personal-dir/ox-reveal.el")
 ;; (load-file "~/.emacs.d/personal/prelude-personal-dir/htmlize.el")
