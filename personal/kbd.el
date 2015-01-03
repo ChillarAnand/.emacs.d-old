3c4b41812208f0cbf3a8dc25f0c8a5c6898a73ac
K3 <anand.r@leucinerichbio.com>
2015-01-03 16:57:26 +0530

    added key chord bindings

--- personal/kbd.el
+++ personal/kbd.el
@@ -14,3 +14,14 @@
 (defun python-mode-settings ()
   (global-set-key (kbd "C-c C-f") 'next-buffer))
 (add-hook 'python-mode 'python-mode-settings)
+
+
+;;; key chord
+(prelude-require-package 'key-chord)
+
+(require 'key-chord)
+
+(key-chord-define-global "ff" 'helm-recentf)
+(key-chord-define-global "pp" 'helm-projectile-switch-project)
+(key-chord-define-global "pf" 'helm-projectile-find-file)
+(key-chord-define-global "bb" 'prelude-switch-to-previous-buffer)
