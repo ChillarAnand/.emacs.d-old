669c60b83cdd0c830093b7cf43df74e6459cbce0
anand <anand21nanda@gmail.com>
2015-07-28 14:59:32 +0530

    updated packages

--- personal/packages.el
+++ personal/packages.el
@@ -18,7 +18,6 @@
     writeroom-mode writegood-mode benchmark-init phi-search key-chord
     which-key virtualenvwrapper helm))
 
-(prelude-require-package 'use-package)
 (require 'use-package)
 
 ;; (load-file "~/projects/lisp/impatient-markup/impatient-markup.el")
@@ -42,42 +41,29 @@
 (elpy-enable)
 (use-package elpy
   :init
-  (progn
-    ;; to export venv
-    ;; (let ((workon-home (expand-file-name "~/.virtualenvs/")))
-    ;;   (setenv "WORKON_HOME" workon-home)
-    ;;   (setenv "VIRTUALENVWRAPPER_HOOK_DIR" workon-home))
-    (setq python-indent-offset 4)
-    (elpy-enable)
-    ;; (elpy-use-ipython)
-    (defalias 'workon 'pyvenv-workon)
-    (setq elpy-test-runner 'elpy-test-pytest-runner)
+  (setq python-indent-offset 4)
+  (setq elpy-test-runner 'elpy-test-pytest-runner)
 
-    (define-key elpy-mode-map (kbd "C-c C-c") 'my/send-region-or-buffer)
-    (defun my/send-region-or-buffer (&optional arg)
-      (interactive "P")
-      (elpy-shell-send-region-or-buffer arg)
-      (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
-        (set-window-point (get-buffer-window (current-buffer))
-                          (point-max))))
+  (define-key elpy-mode-map (kbd "C-c C-c") 'my/send-region-or-buffer)
+  (defun my/send-region-or-buffer (&optional arg)
+    (interactive "P")
+    (elpy-shell-send-region-or-buffer arg)
+    (with-current-buffer (process-buffer (elpy-shell-get-or-create-process))
+      (set-window-point (get-buffer-window (current-buffer))
+                        (point-max))))
 
-    (define-key elpy-mode-map (kbd "C-<right>") nil)
-    (define-key elpy-mode-map (kbd "C-c C-o") nil)
-    (define-key elpy-mode-map (kbd "C-<left>") nil)
-    (define-key elpy-mode-map (kbd "C-c C-c") 'my/send-region-or-buffer)
+  (define-key elpy-mode-map (kbd "C-<right>") nil)
+  (define-key elpy-mode-map (kbd "C-c C-o") nil)
+  (define-key elpy-mode-map (kbd "C-<left>") nil)
+  (define-key elpy-mode-map (kbd "C-c C-c") 'my/send-region-or-buffer)
 
-    (setq elpy-rpc-timeout nil)
-    (append grep-find-ignored-files "flycheck_*")))
+  (setq elpy-rpc-timeout nil)
+  (append grep-find-ignored-files "flycheck_*"))
 
 
 (load-file "~/projects/lisp/real-auto-save/real-auto-save.el")
 (add-hook 'prog-mode-hook 'real-auto-save-mode)
 (setq real-auto-save-interval 4)
-;; (save-sexp-save-setq "~/.emacs.d/personal/variables.el" 'real-auto-save-alist)
-(setq test '(1 2 3))
-(savehist-mode 1)
-;; (eval-after-load "savehist"
-;;   '(add-to-list 'savehist-additional-variables 'test))
 
 
 (use-package multiple-cursors
@@ -88,69 +74,48 @@
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
 
 
-;; (use-package delight
-;;   :init
-;;   (delight '((abbrev-mode " Abv" abbrev)
-;;              (smart-tab-mode " t" smart-tab)
-;;              (eldoc-mode nil "eldoc")
-;;              (paredit-mode " par" paredit)
-;;              (projectile-mode " proj" projectile)
-;;              (emacs-lisp-mode "Elisp" :major)
-;;              (rainbow-mode)
-;;              (flyspell-mode nil flyspell)
-;;              (guru-mode nil guru))))
-
-
 (use-package company
   :init
-  (progn
-    (global-company-mode 1)
+  (global-company-mode 1)
 
-    (setq company-idle-delay 0)
-    (setq company-tooltip-limit 5)
-    (setq company-minimum-prefix-length 1)
-    (setq company-tooltip-flip-when-above t)
+  (setq company-idle-delay 0)
+  (setq company-tooltip-limit 5)
+  (setq company-minimum-prefix-length 1)
+  (setq company-tooltip-flip-when-above t)
 
-    (define-key company-active-map (kbd "M-n") nil)
-    (define-key company-active-map (kbd "M-p") nil)
-    (define-key company-active-map (kbd "C-n") #'company-select-next)
-    (define-key company-active-map (kbd "C-p") #'company-select-previous)))
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 
 (use-package header2
   :init
-  (progn
-    (add-hook 'emacs-lisp-mode-hook 'auto-make-header)))
+  (add-hook 'emacs-lisp-mode-hook 'auto-make-header))
 
 
 (use-package web-mode
   :init
-  (progn
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-js-indent-offset 0)
+  (setq web-mode-script-padding 0)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-expanding t)
+  (setq web-mode-enable-css-colorization t)
 
-    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (set (make-local-variable 'company-backends) '(company-css))
 
-    (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
-
-    (setq web-mode-markup-indent-offset 4)
-    (setq web-mode-code-indent-offset 4)
-    (setq web-mode-css-indent-offset 4)
-    (setq web-mode-js-indent-offset 0)
-    (setq web-mode-script-padding 0)
-
-    (setq web-mode-enable-auto-pairing t)
-    (setq web-mode-enable-auto-expanding t)
-    (setq web-mode-enable-css-colorization t)
-
-    (set (make-local-variable 'company-backends) '(company-css))
-
-    (define-key prelude-mode-map (kbd "C-c C-i") nil)
-    (bind-key "C-c C-i" 'web-mode-buffer-indent)))
+  (define-key prelude-mode-map (kbd "C-c C-i") nil)
+  (bind-key "C-c C-i" 'web-mode-buffer-indent))
 
 
 (use-package company-quickhelp
   :init
-  (progn
-    (company-quickhelp-mode 1)))
+  (company-quickhelp-mode 1))
 
 
 (use-package nyan-mode
@@ -166,76 +131,64 @@
 
 (use-package sx
   :init
-  (progn
-    (require 'sx-load)))
+  (require 'sx-load))
 
 
 (use-package edit-server
   :init
-  (progn
-    (when (require 'edit-server nil t)
-      (setq edit-server-new-frame nil)
-      (edit-server-start))))
+  (when (require 'edit-server nil t)
+    (setq edit-server-new-frame nil)
+    (edit-server-start)))
 
 
 (use-package mysql
   :init
-  (progn
-    (require 'sql)
-    (add-hook 'sql-mode-hook 'sqlup-mode)
-    (sql-set-product "mysql")
+  (require 'sql)
+  (add-hook 'sql-mode-hook 'sqlup-mode)
+  (sql-set-product "mysql")
 
-    (add-hook 'sql-interactive-mode-hook
-              (lambda ()
-                (toggle-truncate-lines t)))
+  (add-hook 'sql-interactive-mode-hook
+            (lambda ()
+              (toggle-truncate-lines t)))
 
-    (load-file "~/.emacs.d/.private.el")
+  (load-file "~/.emacs.d/.private.el")
 
-    (setq sql-connection-alist
-          '((pool-server
-             (sql-server sql-server-address)
-             (sql-user sql-server-user)
-             (sql-password sql-server-password)
-             (sql-database sql-server-database)
-             (sql-port sql-server-port))
+  (setq sql-connection-alist
+        '((pool-server
+           (sql-server sql-server-address)
+           (sql-user sql-server-user)
+           (sql-password sql-server-password)
+           (sql-database sql-server-database)
+           (sql-port sql-server-port))
 
-            (pool-local
-             (sql-server sql-local-server)
-             (sql-user sql-local-user)
-             (sql-password sql-local-password)
-             (sql-database sql-local-database)
-             (sql-port sql-local-port))))
+          (pool-local
+           (sql-server sql-local-server)
+           (sql-user sql-local-user)
+           (sql-password sql-local-password)
+           (sql-database sql-local-database)
+           (sql-port sql-local-port))))
 
-    (defun sql-connect-preset (name)
-      "Connect to a predefined SQL connection listed in `sql-connection-alist'"
-      (eval `(let ,(cdr (assoc name sql-connection-alist))
-               (flet ((sql-get-login (&rest what)))
-                 (sql-product-interactive sql-product)))))
+  (defun sql-connect-preset (name)
+    "Connect to a predefined SQL connection listed in `sql-connection-alist'"
+    (eval `(let ,(cdr (assoc name sql-connection-alist))
+             (flet ((sql-get-login (&rest what)))
+               (sql-product-interactive sql-product)))))
 
-    (defun sql-pool-server ()
-      (interactive)
-      (sql-connect-preset 'pool-server))
+  (defun sql-pool-server ()
+    (interactive)
+    (sql-connect-preset 'pool-server))
 
-    (defun sql-pool-local ()
-      (interactive)
-      (sql-connect-preset 'pool-local))
+  (defun sql-pool-local ()
+    (interactive)
+    (sql-connect-preset 'pool-local))
 
-    (define-key sql-mode-map (kbd "C-c C-c") 'mysql-send-paragraph)
-    (defun mysql-send-paragraph ()
-      (interactive)
-      (sql-send-paragraph)
-      (with-current-buffer (process-buffer (get-process "SQL"))
-        (set-window-point (get-buffer-window (current-buffer))
-                          (point-max))))))
-
-
-(use-package guide-key
-  :init
-  (setq guide-key/idle-delay 0.5)
-  (setq guide-key/popup-window-position 'bottom)
-  (setq guide-key/guide-key-sequence t)
-  (setq guide-key/guide-key-sequence '("<key-chord>"))
-  (guide-key-mode 1))
+  (define-key sql-mode-map (kbd "C-c C-c") 'mysql-send-paragraph)
+  (defun mysql-send-paragraph ()
+    (interactive)
+    (sql-send-paragraph)
+    (with-current-buffer (process-buffer (get-process "SQL"))
+      (set-window-point (get-buffer-window (current-buffer))
+                        (point-max)))))
 
 
 (use-package multi-term
@@ -252,62 +205,58 @@
 
 (use-package electric-case
   :init
-  (progn
+  (defun electric-case-python-init ()
 
-    (defun electric-case-python-init ()
+    (electric-case-mode 1)
+    (setq electric-case-max-iteration 2)
 
-      (electric-case-mode 1)
-      (setq electric-case-max-iteration 2)
+    (setq electric-case-criteria
+          (lambda (b e)
+            (let ((proper (electric-case--possible-properties b e))
+                  (key (key-description (this-single-command-keys))))
+              (cond
+               ((member 'font-lock-variable-name-face proper)
+                ;; #ifdef A_MACRO  /  int variable_name;
+                (if (member '(cpp-macro) (python-guess-basic-syntax)) 'usnake 'snake))
+               ((member 'font-lock-string-face proper) nil)
+               ((member 'font-lock-comment-face proper) nil)
+               ((member 'font-lock-keyword-face proper) nil)
+               ((member 'font-lock-function-name-face proper) 'snake)
+               ((member 'font-lock-type-face proper) 'snake)
+               (electric-case-convert-calls 'snake)
+               (t nil)))))
 
-      (setq electric-case-criteria
-            (lambda (b e)
-              (let ((proper (electric-case--possible-properties b e))
-                    (key (key-description (this-single-command-keys))))
-                (cond
-                 ((member 'font-lock-variable-name-face proper)
-                  ;; #ifdef A_MACRO  /  int variable_name;
-                  (if (member '(cpp-macro) (python-guess-basic-syntax)) 'usnake 'snake))
-                 ((member 'font-lock-string-face proper) nil)
-                 ((member 'font-lock-comment-face proper) nil)
-                 ((member 'font-lock-keyword-face proper) nil)
-                 ((member 'font-lock-function-name-face proper) 'snake)
-                 ((member 'font-lock-type-face proper) 'snake)
-                 (electric-case-convert-calls 'snake)
-                 (t nil)))))
+    (defadvice electric-case-trigger (around electric-case-c-try-semi activate)
+      (when (and electric-case-mode
+                 (eq major-mode 'python-mode)))))
 
-      (defadvice electric-case-trigger (around electric-case-c-try-semi activate)
-        (when (and electric-case-mode
-                   (eq major-mode 'python-mode)))))
-
-    (add-hook 'python-mode-hook 'electric-case-python-init)
-    (setq electric-case-convert-calls t)))
+  (add-hook 'python-mode-hook 'electric-case-python-init)
+  (setq electric-case-convert-calls t))
 
 
 (use-package smart-mode-line
   :init
-  (progn
-    (sml/setup)
-    (sml/apply-theme 'light)
-    (rich-minority-mode 1)))
+  (sml/setup)
+  (sml/apply-theme 'light)
+  (rich-minority-mode 1))
 
 
 (use-package circe
   :init
-  (progn
-    (setq circe-network-options
-          `(("Freenode"
-             :nick "chillaranand"
-             :channels
-             ("#emacs" "#emacs-circe" "#emacs-elpy"
-              "#python-india" "#python-dev"
-              "#dgplug")
-             :nickserv-password ,freenode-password)))
-    (setq circe-reduce-lurker-spam t)))
+  (setq circe-network-options
+        `(("Freenode"
+           :nick "chillaranand"
+           :channels
+           ("#emacs" "#emacs-circe" "#emacs-elpy"
+            "#python-india" "#python-dev"
+            "#dgplug")
+           :nickserv-password ,freenode-password)))
+  (setq circe-reduce-lurker-spam t))
 
 
-;; (use-package wakatime-mode
-;;   :init
-;;   (global-wakatime-mode))
+(use-package wakatime-mode
+  :init
+  (global-wakatime-mode))
 
 (use-package impatient-mode)
 
@@ -331,28 +280,28 @@
   :init
   (global-set-key (kbd "M-;") 'comment-dwim-2))
 
+
 (use-package openwith
   :init
-  (progn
-    (openwith-mode t)
-    (setq large-file-warning-threshold 500000000)
-    (setq openwith-associations
-          (list (list (openwith-make-extension-regexp '("pdf"))
-                      "evince" '(file))
-                (list (openwith-make-extension-regexp '("flac" "mp3" "wav"))
-                      "vlc" '(file))
-                (list (openwith-make-extension-regexp
-                       '("avi" "flv" "mov" "mp4" "mkv" "mpeg" "mpg" "ogg" "wmv"))
-                      "vlc" '(file))
-                (list (openwith-make-extension-regexp '("bmp" "jpeg" "jpg" "png"))
-                      "ristretto" '(file))
-                (list (openwith-make-extension-regexp '("doc" "docx" "odt"))
-                      "libreoffice" '("--writer" file))
-                (list (openwith-make-extension-regexp '("ods" "xls" "xlsx"))
-                      "libreoffice" '("--calc" file))
-                (list (openwith-make-extension-regexp '("odp" "pps" "ppt" "pptx"))
-                      "libreoffice" '("--impress" file))
-                ))))
+  (openwith-mode t)
+  (setq large-file-warning-threshold 500000000)
+  (setq openwith-associations
+        (list (list (openwith-make-extension-regexp '("pdf"))
+                    "evince" '(file))
+              (list (openwith-make-extension-regexp '("flac" "mp3" "wav"))
+                    "vlc" '(file))
+              (list (openwith-make-extension-regexp
+                     '("avi" "flv" "mov" "mp4" "mkv" "mpeg" "mpg" "ogg" "wmv"))
+                    "vlc" '(file))
+              (list (openwith-make-extension-regexp '("bmp" "jpeg" "jpg" "png"))
+                    "ristretto" '(file))
+              (list (openwith-make-extension-regexp '("doc" "docx" "odt"))
+                    "libreoffice" '("--writer" file))
+              (list (openwith-make-extension-regexp '("ods" "xls" "xlsx"))
+                    "libreoffice" '("--calc" file))
+              (list (openwith-make-extension-regexp '("odp" "pps" "ppt" "pptx"))
+                    "libreoffice" '("--impress" file))
+              )))
 
 
 (use-package helm-dired-recent-dirs)
@@ -366,41 +315,40 @@
 
 (use-package helm
   :init
-  (progn
-    (bind-key "C-x r l" 'helm-bookmarks)
+  (bind-key "C-x r l" 'helm-bookmarks)
 
-    (defvar helm-source-emacs-commands
-      (helm-build-sync-source "Emacs commands"
-        :candidates (lambda ()
-                      (let ((cmds))
-                        (mapatoms
-                         (lambda (elt) (when (commandp elt) (push elt cmds))))
-                        cmds))
-        :coerce #'intern-soft
-        :action #'command-execute)
-      "A simple helm source for Emacs commands.")
+  (defvar helm-source-emacs-commands
+    (helm-build-sync-source "Emacs commands"
+      :candidates (lambda ()
+                    (let ((cmds))
+                      (mapatoms
+                       (lambda (elt) (when (commandp elt) (push elt cmds))))
+                      cmds))
+      :coerce #'intern-soft
+      :action #'command-execute)
+    "A simple helm source for Emacs commands.")
 
-    (defvar helm-source-emacs-commands-history
-      (helm-build-sync-source "Emacs commands history"
-        :candidates (lambda ()
-                      (let ((cmds))
-                        (dolist (elem extended-command-history)
-                          (push (intern elem) cmds))
-                        cmds))
-        :coerce #'intern-soft
-        :action #'command-execute)
-      "Emacs commands history")
+  (defvar helm-source-emacs-commands-history
+    (helm-build-sync-source "Emacs commands history"
+      :candidates (lambda ()
+                    (let ((cmds))
+                      (dolist (elem extended-command-history)
+                        (push (intern elem) cmds))
+                      cmds))
+      :coerce #'intern-soft
+      :action #'command-execute)
+    "Emacs commands history")
 
-    (setq helm-mini-default-sources '(helm-source-buffers-list
-                                      helm-source-recentf
-                                      helm-source-dired-recent-dirs
-                                      helm-source-emacs-commands-history
-                                      helm-source-emacs-commands
-                                      helm-chrome-source
-                                      hgs/helm-c-source-stars
-                                      hgs/helm-c-source-repos
-                                      helm-source-buffer-not-found
-                                      hgs/helm-c-source-search))))
+  (setq helm-mini-default-sources '(helm-source-buffers-list
+                                    helm-source-recentf
+                                    helm-source-dired-recent-dirs
+                                    helm-source-emacs-commands-history
+                                    helm-source-emacs-commands
+                                    helm-chrome-source
+                                    hgs/helm-c-source-stars
+                                    hgs/helm-c-source-repos
+                                    helm-source-buffer-not-found
+                                    hgs/helm-c-source-search)))
 
 (use-package phi-search
   :init
@@ -414,10 +362,9 @@
 
 (use-package google-translate
   :init
-  (progn
-    (setq  google-translate-default-source-language "en")
-    (setq  google-translate-default-target-language "kn")
-    (require 'google-translate-default-ui)))
+  (setq  google-translate-default-source-language "en")
+  (setq  google-translate-default-target-language "kn")
+  (require 'google-translate-default-ui))
 
 
 ;; (use-package slime
