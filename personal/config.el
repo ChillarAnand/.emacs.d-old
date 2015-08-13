8277f61afcef50b36025f05d272d6bc72a67ae78
anand <anand21nanda@gmail.com>
2015-08-13 11:56:04 +0530

    c

--- personal/config.el
+++ personal/config.el
@@ -7,6 +7,12 @@
 (setq kill-buffer-query-functions
       (delq 'process-kill-buffer-query-function kill-buffer-query-functions))
 
+(eval-after-load 'comint
+  '(progn
+     (define-key comint-mode-map (kbd "<up>") #'comint-previous-matching-input-from-input)
+     (define-key comint-mode-map (kbd "<down>") #'comint-next-matching-input-from-input)))
+
+
 (provide 'config)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; config.el ends here
